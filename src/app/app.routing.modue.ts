477cdd673edf4dc48e0acc940d59e7fb23daf47d
helenofn <63683746+helenofn@users.noreply.github.com>
2021-01-29 20:05:23 -0300

    Resolvers

--- src/app/app.routing.modue.ts
+++ src/app/app.routing.modue.ts
@@ -4,12 +4,24 @@ import { NotFoundComponent } from './erros/not-found/not-found.component';
 
 import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
 import { PhotoListComponent } from './photos/photo-list/photo-list.component';
-import { PhotoComponent } from './photos/photo/photo.component';
+import { PhotoListResolver } from './photos/photo-list/photo-list.resolver';
 
 const routes: Routes = [
-    {path: 'user/:userName', component: PhotoListComponent},
-    {path: 'p/add', component: PhotoFormComponent},
-    {path: '**', component: NotFoundComponent}
+    {
+        path: 'user/:userName', 
+        component: PhotoListComponent,
+        resolve: {
+            photos: PhotoListResolver
+        }
+    },
+    {
+        path: 'p/add', 
+        component: PhotoFormComponent
+    },
+    {
+        path: '**', 
+        component: NotFoundComponent
+    }
 ];
 
 @NgModule({
--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
 import { Photo } from '../photo/photo';
-import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
@@ -11,18 +10,22 @@ import { PhotoService } from '../photo/photo.service';
 export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
+  filter: string = '';
 
   constructor(
-    private photoService: PhotoService,
     private activectedRoute: ActivatedRoute
   ){}
 
   ngOnInit(): void {
 
-    const userName = this.activectedRoute.snapshot.params.userName;
+    //Foi transferido para o resolver
+    /*const userName = this.activectedRoute.snapshot.params.userName;
     this.photoService
       .listFromUser(userName)
-      .subscribe(photos => this.photos = photos);
+      .subscribe(photos => this.photos = photos);*/
+
+      this.photos = this.activectedRoute.snapshot.data.photos;
+
   }
 
 }
