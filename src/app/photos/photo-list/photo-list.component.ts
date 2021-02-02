4950dcadf52fde1b7fca5c0d9b67c79da69fb294
helenofn <63683746+helenofn@users.noreply.github.com>
2021-02-02 17:46:40 -0300

    card

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,23 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
+
 import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
   photos: Photo[] = [];
   filter: string = '';
+  debounce: Subject<string> = new Subject<string>();
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
 
   constructor(
-    private activectedRoute: ActivatedRoute
+    private activectedRoute: ActivatedRoute,
+    private photoService: PhotoService
   ){}
-
+  
   ngOnInit(): void {
-
+    this.userName = this.activectedRoute.snapshot.params.userName;
+    
     //Foi transferido para o resolver
     /*const userName = this.activectedRoute.snapshot.params.userName;
     this.photoService
@@ -26,6 +36,26 @@ export class PhotoListComponent implements OnInit {
 
       this.photos = this.activectedRoute.snapshot.data.photos;
 
+      this.debounce
+        .pipe(debounceTime(500))
+        .subscribe(filter => this.filter = filter);//Cuidado, se não finalizar ele vai ficar enfilerando os eventos e vai consumir muita memória
+
+  }
+
+  ngOnDestroy(): void {
+    this.debounce.unsubscribe();//Quando sair da página, desctruir o observable do filter, assim não fica enfilerando sempre.
+  }
+
+  load(){
+    this.photoService
+      .listFromUserPaginated(this.userName, this.currentPage++)
+      .subscribe(photos => {
+        //this.photos.push(...photos);//Para cada item de foto ele vai desmembrar os dados // O porblema é que o componente de exinição não vai entender que a lista de photos foi alterada
+        this.photos = this.photos.concat(photos);// O concat gera uma nova lista, é uma nova referencia e o mecanismo de change detection do Angular vai funcionar.
+        if(!photos.length){
+          this.hasMore = false;
+        }
+      });
   }
 
 }
--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,26 +1,27 @@
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
 
-import { PhotoComponent } from './photo/photo.component';
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
-import { FilterByDescription } from './photo-list/filter-by-description.pipe';
+import { PhotoModule } from './photo/photo.module';
+import { PhotoFormModule } from './photo-form/photo-form-module';
+import { PhotoListModule } from './photo-list/photo-list.module';
 
 
 
 @NgModule({
+    /* Depois de ter organizado em submodulos, passamos a importar apenas os submodulos ao inves de todos os componentes
     declarations:[ 
         PhotoComponent, 
         PhotoListComponent, 
         PhotoFormComponent, 
         PhotosComponent,
-        FilterByDescription 
-    ],
+        FilterByDescription,
+        LoadButtonComponent 
+    ],*/
     imports: [ 
-        HttpClientModule,
-        CommonModule
+        PhotoModule,
+        PhotoFormModule,
+        PhotoListModule,
+        /*HttpClientModule, -- Depois de termos organizado em sub-modulos, cada módulo ou submódulo importa o que precisa
+        CommonModule*/
      ]
 })
 export class PhotosModule{
