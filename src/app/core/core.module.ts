cc63d1e02b5cfcd68feff36d9019599416a42ff1
helenofn <63683746+helenofn@users.noreply.github.com>
2021-02-04 13:00:30 -0300

    validação de formulário

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
     declarations: [HeaderComponent],
     exports: [HeaderComponent],
     imports:[
-        CommonModule
+        CommonModule,
+        RouterModule
     ]
 })
 export class CoreModule{
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { SignInComponent } from './signin/signin.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { VMessageComponent } from '../shared/components/vmessage/vmessage.component';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
 import { RouterModule } from '@angular/router';
+import { SignUpComponent } from './signup/signup.component';
 
 @NgModule({
-    declarations:[ SignInComponent ],
+    declarations:[ SignInComponent, SignUpComponent ],
     imports: [
         ReactiveFormsModule, 
+        FormsModule,
         CommonModule, 
         VMessageModule,
         RouterModule
