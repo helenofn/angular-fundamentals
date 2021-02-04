ee36eed71550c95e197f85489926761e2847cc93
helenofn <63683746+helenofn@users.noreply.github.com>
2021-02-04 11:16:51 -0300

    RouterGuard

--- src/app/core/user/user.service.ts
+++ src/app/core/user/user.service.ts
@@ -9,6 +9,7 @@ export class UserService{
 
     //private userSubject = new Subject<User>(); // No nosso cenário da problema no subject, pois este serviço é carregado antes da renderização da navbar, ou seja, quando a navbar é carregada, o subject já foi passado.
     private userSubject = new BehaviorSubject<User>(null);//O behavior emit o valorigual ao subject, mas se ninguem ouviu, ele guarda para quando alguem se inscrever
+    private userName: string;
 
     constructor(private tokenService: TokenService){
         this.tokenService.hasToken() && this.decodAndNotify();
@@ -26,12 +27,21 @@ export class UserService{
     private decodAndNotify(){
         const token = this.tokenService.getToken();
         const user = jwt_decode(token) as User;
+        this.userName = user.name;
         this.userSubject.next(user);
     }
 
     logout(){
         this.tokenService.removeToken();
         this.userSubject.next(null);
+    }
+
+    isLogged(){
+        return this.tokenService.hasToken();
+    }
+
+    getUserName(){
+        return this.userName;
     }
 
 }
