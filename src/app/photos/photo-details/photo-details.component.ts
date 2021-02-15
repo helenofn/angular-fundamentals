5ca953dc7ef12883a8dd76acd907656adcc01cda
helenofn <63683746+helenofn@users.noreply.github.com>
2021-02-15 08:47:46 -0300

    Limpando mensaggem quando muda de rota

--- src/app/photos/photo-details/photo-details.component.ts
+++ src/app/photos/photo-details/photo-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { UserService } from 'src/app/core/user/user.service';
 import { AlertService } from 'src/app/shared/components/alert/alert.service';
 import { Photo } from '../photo/photo';
 import { PhotoComment } from '../photo/photo-comment';
@@ -20,11 +21,17 @@ export class PhotoDetailsComponent implements OnInit{
         private route: ActivatedRoute,
         private photoService: PhotoService,
         private router: Router,
-        private alertService: AlertService){}
+        private alertService: AlertService,
+        private userService: UserService){}
 
     ngOnInit(): void {
         this.photoId = this.route.snapshot.params.photoId;
         this.photo$ = this.photoService.findById(this.photoId);
+        this.photo$.subscribe(
+            () => {}, 
+            err => {
+                this.router.navigate(['not-found']);
+        });
 
         /*this.photoService
             .findById(id)
@@ -38,11 +45,11 @@ export class PhotoDetailsComponent implements OnInit{
             .removePhoto(this.photoId)
             .subscribe(
                 () => {
-                    this.alertService.success('Photo removed');
-                    this.router.navigate(['/']);
+                    this.alertService.success('Photo removed', true);
+                    this.router.navigate(['/user',this.userService.getUserName()]);
                 },
                 err => {
-                    this.alertService.warning('Could not delete the photo');
+                    this.alertService.warning('Could not delete the photo', true);
                 });
     }
     
