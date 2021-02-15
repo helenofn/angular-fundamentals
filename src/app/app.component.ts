d19f45ca86bac6e61a460e7e4e0c1f302dbb6cd8
helenofn <63683746+helenofn@users.noreply.github.com>
2021-02-15 11:57:57 -0300

    Alterando o ttulo de acordo com o titulo colocado em data na rota

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,34 @@
+import { Route } from '@angular/compiler/src/core';
 import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, map, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
   
+  constructor(
+    private router: Router, // Me da acesso ao evento disparado sobre a rota
+    private activateRoute: ActivatedRoute, // Me da acesso a informação que está na rota
+    private titleService: Title
+  ){}
+
+  ngOnInit(): void {
+    
+    //Para cada rota acessada, com oescopo de página, altera para o title da página pelo title informado em "data" na própria rota
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .pipe(map(() => this.activateRoute))
+      .pipe(map(route => {
+        while(route.firstChild) route = route.firstChild;//Necessário para pegar apenas a rota pai
+        return route;
+      }))
+      .pipe(switchMap(route => route.data))
+      .subscribe(event => this.titleService.setTitle(event.title));
+  }
 
 }
